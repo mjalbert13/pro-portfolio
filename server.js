14324709f0f282a721ce418efd10325010791e3d
mjalbert13 <mjalbert13@gmail.com>
2019-11-14 11:50:35 -0500

    Set up started and server running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('API running!'));
+
+//Middleware
+app.use(express.json({ extended: false }))
+
+//App listen function
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
